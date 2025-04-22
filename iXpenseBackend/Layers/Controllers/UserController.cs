62b893b0c41cff17f97b8fcd8da35c3afd8fc1cd
Joakim Lönngren <joakim.lonngren@yh.nackademin.se>
2025-04-22 16:12:33 +0200

    Added functionality of register a user and also the possibility to login.

---
+++ iXpenseBackend/Layers/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using iXpenseBackend.Layers.Services;
+using iXpenseBackend.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iXpenseBackend.Layers.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly UserService _userService;
+
+        public UserController(IConfiguration configuration, UserService userService)
+        {
+            _configuration = configuration;
+            _userService = userService;
+        }
+
+        //Register a user
+        [Route("Register")]
+        [HttpPost]
+        public async Task <IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            var result = await _userService.RegisterUserAsync(registerDto);
+            if (!result.IsSuccess)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", result.Message });
+            }
+            else
+            {
+                return Ok(new { Status = "Created", result.Message, result.UserId });
+            }
+        }
+
+        //Login a user
+        [Route("Login")]
+        [HttpPost]
+        public async Task <IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var result = await _userService.LoginUserAsync(loginDto);
+            if (!result.IsSuccess)
+            {
+                return Unauthorized(new { Status = "Error", result.Message });
+            }
+            return Ok(new { Status = "Success", result.Message, result.Token });
+        }
+    }
+}
--- iXpenseBackend/Layers/Services/UserService.cs
+++ iXpenseBackend/Layers/Services/UserService.cs
@@ -1,6 +1,94 @@
-﻿namespace iXpenseBackend.Layers.Services
+﻿using iXpenseBackend.Layers.Repositories;
+using iXpenseBackend.Models.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace iXpenseBackend.Layers.Services
 {
     public class UserService
     {
+        private readonly UserRepo _userRepo;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+
+        public UserService(UserRepo userRepo, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IConfiguration configuration)
+        {
+            _userRepo = userRepo;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        //Register a new user
+        public async Task<(bool IsSuccess, string Message, string? UserId)> RegisterUserAsync(RegisterDto registerDto)
+        {
+            var userExists = await _userRepo.FindUserByUsernameAsync(registerDto.Username);
+            if(userExists != null)
+            {
+                return (false, "User already exists", null);
+            }
+
+            var user = new IdentityUser
+            {
+                Email = registerDto.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerDto.Username,
+            };
+
+            var result = await _userRepo.CreateUserAsync(user, registerDto.Password);
+            if(result.Succeeded)
+            {
+                return (true, "User created successfully", user.Id);
+            }
+            else
+            {
+                return (false, "Something went wrong, make sure you put in the correct details", null);
+            }
+
+        }
+
+        //Login a user
+        public async Task <(bool IsSuccess, string Message, string? Token)> LoginUserAsync(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByNameAsync(loginDto.Username);
+            if(user == null)
+            {
+                return (false, "User not found", null);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(loginDto.Username, loginDto.Password, false, false);
+            if (!result.Succeeded)
+            {
+                return (false, "Invalid login credentials", null);
+            }
+
+            var token = GenerateJwtToken(user);
+            return (true, "Login successful", token);
+        }
+
+        //JWT
+        private string GenerateJwtToken(IdentityUser user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtConfig:Secret"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+            
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
