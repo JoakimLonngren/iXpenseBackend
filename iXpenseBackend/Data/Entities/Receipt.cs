6f78b1ec85f7b2287eac492808f9ab9dec22aeb1
Joakim Lönngren <joakim.lonngren@yh.nackademin.se>
2025-04-22 12:20:52 +0200

    Initial setup of project

---
+++ iXpenseBackend/Data/Entities/Receipt.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace iXpenseBackend.Data.Entities
+{
+    public class Receipt
+    {
+        [Key]
+        public int Id { get; set; }
+        public string From { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime Date {  get; set; } 
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+        public ICollection<Item> Items { get; set; } = new List<Item>();
+    }
+}
--- iXpenseBackend/Program.cs
+++ iXpenseBackend/Program.cs
@@ -1,6 +1,68 @@
+using Microsoft.EntityFrameworkCore;
+using iXpenseBackend.Data;
+using iXpenseBackend.Data.DbContext;
+using Microsoft.AspNetCore.Identity;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using iXpenseBackend.Layers.Repositories;
+using iXpenseBackend.Layers.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Cors to allow access from my React-proj
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+        });
+});
+
+//To allow my DB to access the value of my connection-string
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+
+//Allow identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<Context>()
+    .AddDefaultTokenProviders();
+
+//JWT authentication
+var jwtSecret = builder.Configuration["JwtConfig:Secret"];
+var key = Encoding.ASCII.GetBytes(jwtSecret);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(jwt =>
+{
+    jwt.RequireHttpsMetadata = true;
+    jwt.SaveToken = true;
+    jwt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        RequireExpirationTime = true
+    };
+});
+
+//DI for my services and repositories
+builder.Services.AddScoped<CategoryRepo>();
+builder.Services.AddScoped<CategoryService>();
+builder.Services.AddScoped<ReceiptRepo>();
+builder.Services.AddScoped<ReceiptService>();
+builder.Services.AddScoped<UserRepo>();
+builder.Services.AddScoped<UserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +70,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("AllowReactApp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -19,6 +83,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllers();
 
